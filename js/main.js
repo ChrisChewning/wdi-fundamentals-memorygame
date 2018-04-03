932f7e620593bd71f48893249061155c07ef5868
Chris_Chewning <e130814@rrisd.local>
2018-04-03 09:50:05 -0500

    created game logic

--- js/main.js
+++ js/main.js
@@ -1,9 +1,20 @@
 console.log("Up and running!");
 
-var cardOne = "queen";
-var cardTwo = "king";
-var cardThree = "queen";
-var cardFour = "king";
+var cards = ["queen", "queen", "king", "king"];
+var cardsInPlay = []; //empty array for data to go in.
 
-console.log("User flipped " + cardOne);
-console.log("User flipped " + cardThree);
+var cardOne = cards[0];  //new variable to store "queen" in. This way we can retrieve it.
+var cardTwo = cards[1];
+
+cardsInPlay.push(cardOne); //should push "queen" (cardOne's data) into cardsInPlay
+cardsInPlay.push(cardTwo);
+
+console.log("User flipped " + cardsInPlay);
+console.log("User flipped " + cardsInPlay);
+
+if (cardsInPlay[0] === cardsInPlay[1]) {
+  alert("You found a match!");
+}
+else {
+  alert("Sorry, try again.");
+}
