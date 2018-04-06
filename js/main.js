22cb2265e7fbdfe8448ef8131051b42910ec1b3a
Chris_Chewning <e130814@rrisd.local>
2018-04-06 12:42:29 -0500

    Created game logic

--- js/main.js
+++ js/main.js
@@ -1,5 +1,27 @@
 //VAR SECTION
-var cards = ["queen", "queen", "king", "king"];
+var cards = [
+  {
+  rank: "queen",
+  suit: "hearts",
+  image: "images/queen-of-hearts.png"
+},
+{
+  rank: "queen",
+  suit: "diamonds",
+  image: "images/queen-of-diamonds.png"
+},
+{
+  rank: "king",
+  suit: "hearts",
+  images: "images/king-of-hearts.png"
+},
+{
+  rank: "king",
+  suit: "diamonds",
+  images: "images/king-of-diamonds.png"
+}
+];
+
 var cardsInPlay = [];
 //VAR 1: Stores initial data you put in.
 /*VAR 2: This is an empty array, created for data to go in. When a user flips
@@ -11,8 +33,7 @@ a card, the data can go here. */
 var checkForMatch = function() { //function 1: checks to see if cards are equal.
   if (cardsInPlay[0] === cardsInPlay[1]) { //you flip 2 cards and compare them.
     console.log("You found a match!");
-  }
-  else {
+  } else {
     console.log("Sorry, try again.");
   }
 }; //this closes the checkForMatch function.
@@ -22,14 +43,25 @@ var checkForMatch = function() { //function 1: checks to see if cards are equal.
 
 //FUNCTION 2: User flips the card. cardId is your parameter.
 var flipCard = function (cardId) {
-  console.log("User flipped " + cards[cardId]); //shows what card was flipped.
- cardsInPlay.push(cards[cardId]); //add card to the array of cards in play.
+  console.log("User flipped " + cards[cardId].rank); //pushes the name of the card in cardsInPlay rank.
+ cardsInPlay.push(cards[cardId].rank); //add card to the array of cards in play.
 
   if (cardsInPlay.length === 2) {  // Check to see if two cards have been played
     // If so, call the checkForMatch function
     checkForMatch();
-}
-};
+    console.log(cards[cardId].suit); //card image
+    console.log(cards[cardId].image); //card suit
+
+    // Check to see if two cards have been played
+      if (cardsInPlay.length === 2) {
+        // If so, call the checkForMatch function
+        checkForMatch();
+        // Empty cards in play array for next try
+        cardsInPlay = [];
+      }
+      }
+    };
+
 
 flipCard(0);  //flips the first card
 flipCard(1); //flips the third card
